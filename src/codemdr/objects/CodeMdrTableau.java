ef0f8a5117160747db479fe6b8284beca79abae2
Mathis Laroche <ecomath360@gmail.com>
2023-01-30 13:21:30 -0500

    AJOUT - Tout est un objet.
    AJOUT - accéder et changer la valeur d'une propriété d'un objet

--- src/codemdr/objects/CodeMdrTableau.java
+++ src/codemdr/objects/CodeMdrTableau.java
@@ -3,17 +3,21 @@ package codemdr.objects;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
 
 public class CodeMdrTableau extends CodeMdrObj<ArrayList<CodeMdrObj<?>>> {
 
     public CodeMdrTableau(@NotNull ArrayList<CodeMdrObj<?>> value) {
-        super(value);
+        super(value, Map.ofEntries(
+                Map.entry("Taille", new CodeMdrInt(value.size()))
+        ));
     }
 
     public void setValeur(int position, CodeMdrObj<?> valeur) {
         getValue().set(position, valeur);
     }
+
     public CodeMdrObj<?> getValeur(int position) {
         return getValue().get(position);
     }
